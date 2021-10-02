3b00d201cc5bf0ec6bb1ed26b33f95e822779873
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-10-02 23:54:05 +0900

    add CreateTodo sample

--- src/app/pages/graphql/graphql.component.ts
+++ src/app/pages/graphql/graphql.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService, CreateTodoInput } from 'src/app/API.service';
 
 @Component({
   selector: 'app-graphql',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphql.component.scss'],
 })
 export class GraphqlComponent implements OnInit {
-  constructor() {}
+  constructor(private apiService: APIService) {}
 
   ngOnInit(): void {}
+
+  async onClickCreateTodo() {
+    const input: CreateTodoInput = {
+      name: 'first',
+      description: 'first todo',
+    };
+    this.apiService.CreateTodo(input);
+  }
 }
